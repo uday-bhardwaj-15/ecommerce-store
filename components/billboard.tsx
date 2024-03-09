7622d2d1bef4826c763655c677ecd2fe3265ab15
uday-bhardwaj-15 <142799565+uday-bhardwaj-15@users.noreply.github.com>
2024-03-09 20:25:55 +0530

    Update billboard.tsx

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -4,7 +4,7 @@ interface BillboardProps {
   data: BillboardType;
 }
 export const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  console.log(data);
+  
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
